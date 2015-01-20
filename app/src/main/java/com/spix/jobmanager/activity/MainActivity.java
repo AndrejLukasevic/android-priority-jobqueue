8efa004d6d30ea7187aeebeb41af1207a25c7419
AndrejLukasevic <hexagonx@gmail.com>
2015-01-20 21:32:15 +0200

    Activity added

---
+++ app/src/main/java/com/spix/jobmanager/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.spix.jobmanager.activity;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+}
