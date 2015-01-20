9f06612c296359f70a64b050c468fb8c36ff013c
AndrejLukasevic <hexagonx@gmail.com>
2015-01-20 21:01:21 +0200

    refactored to grandle project

---
+++ jobmanagerlib/src/main/java/com/spix/jobqueue/log/CustomLogger.java
@@ -0,0 +1,16 @@
+package com.spix.jobqueue.log;
+
+/**
+ * You can provide your own logger implementation to {@link com.spix.jobqueue.JobManager}
+ * it is very similar to Roboguice's logger
+ */
+public interface CustomLogger {
+    /**
+     * JobManager may call this before logging sth that is (relatively) expensive to calculate
+     * @return
+     */
+    public boolean isDebugEnabled();
+    public void d(String text, Object... args);
+    public void e(Throwable t, String text, Object... args);
+    public void e(String text, Object... args);
+}
